0e4bbde3e64c43e0c1ef63408ccacda23e006934
LuiFeAn <luisfernandogvv@gmail.com>
2024-09-05 19:46:24 -0300

    feat: domain service

---
+++ server/src/domain/services/shoop-keeper.service.ts
@@ -0,0 +1,18 @@
+import { monthInserter } from "../../utils/month-inserter";
+import ShopKeeper from "../entities/user/shoop-keeper.entity";
+import { SignaturePlanEnum } from "../entities/user/signature";
+
+export class ShoopKeeperService {
+  signaturePeriod(shoopKeeper: ShopKeeper) {
+    const { plan, startDate } = shoopKeeper.signature;
+
+    const signaturePeriodLiterals = {
+      [SignaturePlanEnum.STARTER]: monthInserter(startDate, 1),
+      [SignaturePlanEnum.BASIC]: monthInserter(startDate, 6),
+      [SignaturePlanEnum.PREMIUM]: monthInserter(startDate, 12),
+    };
+    const endDate = signaturePeriodLiterals[plan];
+
+    shoopKeeper.signature.insertEndDate(endDate);
+  }
+}
