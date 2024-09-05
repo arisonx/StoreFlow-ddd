2be2498c28eceae35f60d15882672580ebdff238
LuiFeAn <luisfernandogvv@gmail.com>
2024-09-05 19:50:30 -0300

    refactor: rm unused method

--- server/src/domain/entities/user/shoop-keeper.entity.ts
+++ server/src/domain/entities/user/shoop-keeper.entity.ts
@@ -1,42 +1,37 @@
-import Store from "../store/store.entity";
-import Signature from "./signature";
-import User, { IUserCreationProps } from "./user.entity";
-
+import Store from '../store/store.entity'
+import Signature from './signature'
+import User, { IUserCreationProps } from './user.entity'
 interface IShoopKeeperCreationProps extends IUserCreationProps {
-  store: Store;
-  signature: Signature;
+  store: Store
+  signature: Signature
 }
 
 export default class ShopKeeper extends User {
-  private _store: Store;
-  private _signature: Signature;
+  private _store: Store
+  private _signature: Signature
 
   constructor(props: IShoopKeeperCreationProps) {
-    super(props);
-    this._signature = props.signature;
-    this._store = props.store;
-    this.validate();
+    super(props)
+    this._signature = props.signature
+    this._store = props.store
+    this.validate()
   }
 
   private validate() {
     if (!this._store) {
-      throw new Error("Store is Required");
+      throw new Error('Store is Required')
     }
 
     if (!this._signature) {
-      throw new Error("Signature is Required");
+      throw new Error('Signature is Required')
     }
   }
 
-  insertDueDate(date: Date){
-    
-  }
-
   get signature() {
-    return this._signature;
+    return this._signature
   }
 
   get store() {
-    return this._store;
+    return this._store
   }
 }
