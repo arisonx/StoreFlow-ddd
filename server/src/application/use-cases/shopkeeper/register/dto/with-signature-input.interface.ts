a6af4d967b79cf54c4333b325ac748f23dbc4342
luifean <luisfernandogvv@gmail.com>
2024-09-30 11:26:41 -0300

    feat: new use cases

---
+++ server/src/application/use-cases/shopkeeper/register/dto/with-signature-input.interface.ts
@@ -0,0 +1,4 @@
+import { IWithCredentialsAndSignatureInputProps } from 'domain/entities/user/factories/shoop-keeper.facotry'
+
+export default interface IRegisterShopKeeperWithSignatureInputProps
+  extends IWithCredentialsAndSignatureInputProps {}
--- server/src/application/use-cases/shopkeeper/register/register-shopkeeper.use-case.ts
+++ server/src/application/use-cases/shopkeeper/register/register-shopkeeper.use-case.ts
@@ -1,19 +1,16 @@
-import ConflictError from 'domain/base/errors/conflict-error'
-import ShopKeeperFactory from '../../../../domain/entities/user/factories/shoop-keeper.facotry'
-import IShopKeeperRepository from '../../../../domain/repositories/shopkeeper-repository.abstract'
-import { ShopKeeperMapper } from '../shop-keeper.mapper'
-import IRegisterShopKeeperWithContractInputProps from './dto/input.interface'
-import EncryptContract from 'application/contracts/encrypt.interface'
 import CommonUseCase from 'application/@shared/base.use-case'
-export default class RegisterShopKeeperWithContractUseCase extends CommonUseCase {
-  constructor(
-    private readonly shopKeeperRepo: IShopKeeperRepository,
-    private readonly encryptContract: EncryptContract,
-  ) {
+import ConflictError from 'domain/base/errors/conflict-error'
+import IShopKeeperRepository from 'domain/repositories/shopkeeper-repository.abstract'
+export interface IRegisterShopKeeperInputDto {
+  email: string
+  cpf: string
+}
+export class RegisterShopKeeperUseCase extends CommonUseCase {
+  constructor(private readonly shopKeeperRepo: IShopKeeperRepository) {
     super()
   }
 
-  async execute(dto: IRegisterShopKeeperWithContractInputProps) {
+  async execute(dto: IRegisterShopKeeperInputDto) {
     const promises = [
       this.shopKeeperRepo.emailAlreadyExists(dto.email),
       this.shopKeeperRepo.cpfAlreadyExists(dto.cpf),
@@ -36,13 +33,5 @@ export default class RegisterShopKeeperWithContractUseCase extends CommonUseCase
     }
 
     this.notification.issue()
-
-    const hashedPassword = await this.encryptContract.hash(dto.password)
-    dto.password = hashedPassword
-
-    const shopKeeper = ShopKeeperFactory.withContract(dto)
-
-    await this.shopKeeperRepo.create(shopKeeper)
-    return ShopKeeperMapper.toOutput(shopKeeper)
   }
 }
